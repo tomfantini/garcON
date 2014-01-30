e3d8c491718d714b3bc12b25584f631a8e9a9403
Cleyton Fantini <cleyton.fantini@gmail.com>
2014-01-30 21:38:25 -0200

    Entidades

---
+++ Source/GarcON/GarcON/Model/Cardapio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GarcON.Model
+{
+    public class Cardapio
+    {
+        public int id { get; set; }
+        public List<Produto> produto { get; set; }
+        public ProdutoCategoria categoria { get; set; }
+
+    }
+}
---
+++ Source/GarcON/GarcON/Model/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GarcON.Model
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public double preco { get; set; }
+    }
+}
