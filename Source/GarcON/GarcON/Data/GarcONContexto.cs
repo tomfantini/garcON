2bbf209fddbd3a79189de0a8b8afa506250484b1
Gabriel <gabreguarnieri@gmail.com>
2014-02-03 22:42:59 -0200

    CodeFirst

---
+++ Source/GarcON/GarcON/Data/GarcONContexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using GarcON.Model;
+
+namespace GarcON.Data
+{
+    public class GarcONContexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
