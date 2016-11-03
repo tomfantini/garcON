91cc664bea6b06f45b6a3a84350b848a522b8849
tomfantini <cleyton.fantini@gmail.com>
2016-11-03 15:35:10 -0200

    mvc

---
+++ garcON/garcON/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using garcON.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace garcON.Controllers
+{
+    public class CadastroController : Controller
+    {
+
+        private readonly garcONEntities context = new garcONEntities();
+
+        // GET: Cadastro
+        public ActionResult Cadastro()
+        {
+            return View(context.TB_PESSOA.ToList());
+        }
+    }
+}
